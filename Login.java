d295a121c47fdbd1fe09ee20ffaf8033efcd6641
Salinee <nayaksalinee@gmail.com>
2022-09-27 09:40:31 +0530

    adding loginfile

---
+++ Login.java
@@ -0,0 +1,10 @@
+package Test;
+import org.openqa.selenium.*;
+public class Login {
+
+	public static void main(String[] args) {
+		System.getProperty("webdriver.chrome.drivere","C:/Users/salin/Downloads/selenium-java-4.4.0.zip");
+		Webdriver driver=new ChromeDriver();
+	}
+
+}
